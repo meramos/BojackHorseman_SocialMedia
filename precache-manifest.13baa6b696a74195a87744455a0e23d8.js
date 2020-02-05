6df2f3f202484440913fb2ea3bfbcb297d781a84
Maria Ramos <mariaeramosmorales@gmail.com>
2020-02-04 20:41:05 -0400

    Updates

---
+++ precache-manifest.13baa6b696a74195a87744455a0e23d8.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45a90040aced33c9fd5b25ad722f69f3",
+    "url": "/BojackHorseman_SocialMedia/index.html"
+  },
+  {
+    "revision": "22bcbc6a6627ceac0c41",
+    "url": "/BojackHorseman_SocialMedia/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "760d58a30d57af08a49f",
+    "url": "/BojackHorseman_SocialMedia/static/css/main.ae533d13.chunk.css"
+  },
+  {
+    "revision": "22bcbc6a6627ceac0c41",
+    "url": "/BojackHorseman_SocialMedia/static/js/2.e533deee.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/BojackHorseman_SocialMedia/static/js/2.e533deee.chunk.js.LICENSE"
+  },
+  {
+    "revision": "760d58a30d57af08a49f",
+    "url": "/BojackHorseman_SocialMedia/static/js/main.7b6810f4.chunk.js"
+  },
+  {
+    "revision": "ae606502927758f420cd",
+    "url": "/BojackHorseman_SocialMedia/static/js/runtime-main.97d00854.js"
+  },
+  {
+    "revision": "11980d32724044a6da770c3bfd1a88a0",
+    "url": "/BojackHorseman_SocialMedia/static/media/ad_1.11980d32.png"
+  },
+  {
+    "revision": "d2e37b8534756fdaaa4c4dd7a4c6831a",
+    "url": "/BojackHorseman_SocialMedia/static/media/facefeed_video.d2e37b85.png"
+  },
+  {
+    "revision": "152d868c80b5c2182803a2370117c8ed",
+    "url": "/BojackHorseman_SocialMedia/static/media/pickles_purrfect_video.152d868c.png"
+  },
+  {
+    "revision": "17334b1c294f617e75dd5423685df4f7",
+    "url": "/BojackHorseman_SocialMedia/static/media/profilepic_youzone.17334b1c.png"
+  },
+  {
+    "revision": "cd7c1ebf58334d7638476cbc3024e5d4",
+    "url": "/BojackHorseman_SocialMedia/static/media/thumbnail1.cd7c1ebf.png"
+  },
+  {
+    "revision": "cd017b10a1266f1844b706e057794c26",
+    "url": "/BojackHorseman_SocialMedia/static/media/thumbnail2.cd017b10.png"
+  },
+  {
+    "revision": "a48e2e06019f4d1932b847aa169fe58b",
+    "url": "/BojackHorseman_SocialMedia/static/media/thumbnail3.a48e2e06.png"
+  },
+  {
+    "revision": "1980123d5e8d2fe0c79b43ca5b017a4b",
+    "url": "/BojackHorseman_SocialMedia/static/media/youzone.1980123d.png"
+  }
+]);
